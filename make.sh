35dab4e6ae8c5d041a954b6917c303e3d1f8e4d1
haosenwei <961970674@qq.com>
2020-03-23 14:23:12 +0800

    a

---
+++ make.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+BUILD_ID=dontKillMe
+echo "build $1:$2"
+version=v$2
+if test  'master' != $1
+then
+  version="$1-v$2"
+fi
+echo $version
+docker build -t g4m-jenkins:${version} .
+echo `docker images -a| grep g4m-jenkins | grep ${version} | awk '{print $3}' `
+docker login --username=郝森伟 registry-vpn.cn-beijing.aliyuncs.com -p hao189108
+docker tag `docker images -a| grep g4m-jenkins | grep ${version} | awk '{print $3}' ` registry-vpn.cn-beijing.aliyuncs.com/haosenwei/g4m-jenkins:${version}
+docker push registry-vpn.cn-beijing.aliyuncs.com/haosenwei/g4m-jenkins:${version}
