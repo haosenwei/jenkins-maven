9aaaf2a5bfe64128cf766a135bcb584b0a465e6d
haosenwei <haosenwei@dubmic.com>
2021-02-03 18:13:29 +0800

    1

--- make.sh
+++ make.sh
@@ -10,11 +10,11 @@ echo $version
 
 docker login --username=郝森伟 registry.cn-beijing.aliyuncs.com -p hao189108
 
-docker build -t g4m-tomcat:${version} .
-echo `docker images -a| grep g4m-tomcat | grep ${version} | awk '{print $3}' `
+docker build -t g4m-jenkins:${version} .
+echo `docker images -a| grep g4m-jenkins | grep ${version} | awk '{print $3}' `
 
-docker tag g4m-tomcat:${version} registry.cn-beijing.aliyuncs.com/haosenwei/g4m-tomcat:${version}
-docker push registry.cn-beijing.aliyuncs.com/haosenwei/g4m-tomcat:${version}
+docker tag g4m-jenkins:${version} registry.cn-beijing.aliyuncs.com/haosenwei/g4m-jenkins:${version}
+docker push registry.cn-beijing.aliyuncs.com/haosenwei/g4m-jenkins:${version}
 
-docker tag g4m-tomcat:${version} registry.cn-beijing.aliyuncs.com/haosenwei/g4m-tomcat:latest
-docker push registry.cn-beijing.aliyuncs.com/haosenwei/g4m-tomcat:latest
+docker tag g4m-jenkins:${version} registry.cn-beijing.aliyuncs.com/haosenwei/g4m-jenkins:latest
+docker push registry.cn-beijing.aliyuncs.com/haosenwei/g4m-jenkins:latest
